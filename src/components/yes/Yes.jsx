d6edf1be31c6911e7fd1310ddb0f6cf7318fa26d
CandyTale55 <candytale55@gmail.com>
2025-07-08 13:54:36 +0200

    feat: add state variable with React.useSState()
    Replace hard-coded "Yes" on the page with state using React.useState
    To be changed later, it has an index to avoid the error "Functions are
    not valid as a React child."

--- src/components/yes/Yes.jsx
+++ src/components/yes/Yes.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './assets/styles.css'
 
+
+/**
+     * Challenge: Replace our hard-coded "Yes" on the page with 
+     * some state initiated with React.useState()
+     */
+
 export default function Yes() {
-    const result = React.useState("Yes")
+    const result = React.useState("?")
     console.log(result)
     return (
         <main>
