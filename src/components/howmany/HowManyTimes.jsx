048b69410c4d4926036351f109f9a5696f09374c
CandyTale55 <candytale55@gmail.com>
2025-07-09 14:53:54 +0200

    feat: add add functionality to + button
     Challenge:
       See if you can think of a way to add 1 to the count
       every time the + button is clicked

--- src/components/howmany/HowManyTimes.jsx
+++ src/components/howmany/HowManyTimes.jsx
@@ -3,14 +3,15 @@ import { useState } from 'react'
 
 /**
  * Challenge: 
- * Create a function called `add` that runs
- * when the + button is clicked. (Can just console.log("add") for now)
+ * See if you can think of a way to add 1 to the count
+ * every time the + button is clicked
  */
 
 export default function HowManyTimes() {
 
   function add() {
     console.log("add");
+    setCount( count + 1 )  // Don't use ever count++ or ++count because these modify the value directly - Big NONO in React
   }
 
   let [count, setCount] = useState(0);
