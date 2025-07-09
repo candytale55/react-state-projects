54dbb36e28fe52a247d2bc1b9c64f83ea6b92663
CandyTale55 <candytale55@gmail.com>
2025-07-09 14:49:48 +0200

    feat: add add function
      Challenge:
        Create a function called `add` that runs
        when the + button is clicked. (Can just console.log("add") for now)

--- src/components/howmany/HowManyTimes.jsx
+++ src/components/howmany/HowManyTimes.jsx
@@ -2,21 +2,29 @@ import './assets/styles.css'
 import { useState } from 'react'
 
 /**
-    * Challenge: 
-    * Create state to track our count value (initial value is 0)
-    * Don't forget to replace the hard-coded "0" with your new state
-    */
+ * Challenge: 
+ * Create a function called `add` that runs
+ * when the + button is clicked. (Can just console.log("add") for now)
+ */
 
 export default function HowManyTimes() {
-    
-  let [count, setCount] = useState(10);
+
+  function add() {
+    console.log("add");
+  }
+
+  let [count, setCount] = useState(0);
     return (
       <main className="container">
         <h1>How many times will Bob say "state" in this section?</h1>
         <div className="counter">
           <button className="minus" aria-label="Decrease count">–</button>
-          <h2 className="count">{count+1}</h2>
-          <button className="plus" aria-label="Increase count">+</button>
+          <h2 className="count">{count}</h2>
+          <button
+            className="plus"
+            aria-label="Increase count"
+            onClick={add}
+          >+</button>
         </div>
       </main>
     )
