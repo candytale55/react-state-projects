a80e384d1950a2d226fbe190103f0addb9bf4513
CandyTale55 <candytale55@gmail.com>
2025-07-09 20:22:27 +0200

    chore: setup files for isGoingOut w/ternary

---
+++ src/components/isgoingout/IsGoingOut.jsx
@@ -0,0 +1,28 @@
+
+import './assets/styles.css'
+
+
+
+export default function IsGoingOut() {
+    /**
+     * Challenge: Replace the if/else below with a ternary
+     * to determine the text that should display on the page
+     */
+    const isGoingOut = true
+
+    let answer  // 👈 Use ternary here
+
+    // Remove the code below 👇 once your ternary is done
+    if (isGoingOut === true) {
+        answer = "Yes"
+    } else {
+        answer = "No"
+    }
+
+    return (
+        <main>
+            <h1 className="title">Do I feel like going out tonight?</h1>
+            <button className="value">{answer}</button>
+        </main>
+    )
+}
