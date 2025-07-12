0edf3dd0132c4705bdc171d3cbabf1b13ebcb2fc
CandyTale55 <candytale55@gmail.com>
2025-07-12 10:18:44 +0200

    feat: create Count component, replace it in HMT
    /**
     * Challenge:
      * - Create a new component called `Count`
       *    - It should receive a prop called `number`, whose value
        *      is the current value of our count
         *    - Have the component render the h2.count element below
          *      and display the incoming prop `number`
           * - Replace the h2.count below with an instance of
            *   the new Count component, passing the correct value
    	 *   to its `number` prop.
    	  * - After doing this, everything should be working the
    	   *   same as before.
    	    */

---
+++ src/components/howmany/Count.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Count(props) {
+    
+    
+    return (
+        <>
+            <h2 className="count">{props.number}</h2>     
+        </>
+    )
+}
--- src/components/howmany/HowManyTimes.jsx
+++ src/components/howmany/HowManyTimes.jsx
@@ -1,21 +1,36 @@
 import './assets/styles.css'
 import { useState } from 'react'
+import Count from './Count'
 
-// Challenge: update `substract` to use a callback function
+/**
+ * Challenge:
+ * - Create a new component called `Count`
+ *    - It should receive a prop called `number`, whose value
+ *      is the current value of our count
+ *    - Have the component render the h2.count element below
+ *      and display the incoming prop `number`
+ * - Replace the h2.count below with an instance of
+ *   the new Count component, passing the correct value
+ *   to its `number` prop.
+ * - After doing this, everything should be working the
+ *   same as before.
+ */
+
 
 export default function HowManyTimes() {
-  
+
+
   function add() {
     console.log("add")
-    setCount( prevCount => prevCount + 1)
+    setCount(prevCount => prevCount + 1)
   }
-
 
 
   function subtract() {
     console.log("subtract")
     setCount(prevCount => prevCount - 1)
   }
+
 
   let [count, setCount] = useState(0);
     return (
@@ -27,7 +42,9 @@ export default function HowManyTimes() {
             aria-label="Decrease count"
             onClick={subtract}
           >–</button>
-          <h2 className="count">{count}</h2>
+
+          <Count number={count} />
+          
           <button
             className="plus"
             aria-label="Increase count"
