b4b3253df93393018a50020794964d358003f350
CandyTale55 <candytale55@gmail.com>
2025-07-12 11:57:42 +0200

    log a message to verify when component renders

--- src/components/howmany/Count.jsx
+++ src/components/howmany/Count.jsx
@@ -2,10 +2,10 @@ import React from "react"
 
 export default function Count(props) {
     
-    
+    console.log("count rendered")
     return (
-        <>
+
             <h2 className="count">{props.number}</h2>     
-        </>
+      
     )
 }
