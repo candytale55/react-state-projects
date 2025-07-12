6ebf6b8c19497dbe2ea9851c3effb9e9be93674d
CandyTale55 <candytale55@gmail.com>
2025-07-12 02:02:00 +0200

    add radiobuttons and labels with htmlFor

--- src/components/signupform/SignUpForm.jsx
+++ src/components/signupform/SignUpForm.jsx
@@ -5,16 +5,20 @@ import './assets/styles.css'
 
 export default function SignUpForm() {
 
-    /**
-     * Challenge: get the password from the form too and log
-     * it to the console to be sure it came in correctly.
-     */
+  /**
+   * Challenge: grab the employment status from the form and log it
+   * to the console. (Remember to select one of the radios before submitting)
+   * 
+   * Note: This won't work the way you might expect quite yet!
+   */
 
     function signUp(formData) {
         const email = formData.get("email")
         console.log(email)
         const password = formData.get("password")
         console.log(password)
+        const employmentStatus = formData.get("employmentStatus")
+        console.log(employmentStatus)
     }
 
 
@@ -28,9 +32,28 @@ export default function SignUpForm() {
                 <label htmlFor="password">Password: </label>
                 <input id="password" type="password" name="password" />
                 <br />
-
                 <label htmlFor="description">Description: </label>
                 <textarea name="description" id="description"></textarea>
+
+                <br />
+
+          <fieldset>
+            <legend>Employment Status</legend>
+                  <label htmlFor="radioButton">
+                      <input id="radioButton1" type="radio" name="employmentStatus" value="unemployed" />
+                      Unemployed
+                  </label>
+                  <label htmlFor="radioButton">
+                      <input id="radioButton2" type="radio" name="employmentStatus" value="full-time" defaultChecked />
+                      Full-time
+                  </label>
+                  <label htmlFor="radioButton">
+                      <input id="radioButton3" type="radio" name="employmentStatus" value="part-time" />
+                      Part-time
+                  </label>
+                </fieldset>
+                
+                <br />
                 <button>Submit</button>
 
             </form>
