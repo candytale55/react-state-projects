dea11cbdd8fd9d56f802c5e913f4bdb1b43d6a5c
CandyTale55 <candytale55@gmail.com>
2025-07-12 05:03:34 +0200

    display no unread messages if there aren't

--- src/components/unreadmessages/UnreadMessages.jsx
+++ src/components/unreadmessages/UnreadMessages.jsx
@@ -6,13 +6,15 @@ export default function UnreadMessages() {
 
     /**
      * Challenge:
-     * Only display the <h1> below if there are unread messages
-     * Display the number of unread messages
+     * If there are 0 unread messages, display a paragraph that says "You
+     * have no unread messages". (So, the logic will be the opposite of
+     * what we have for the h1)
      */
 
     return (
         <div className="container">
-            { unreadMessages.length > 0 && <h1>You have {unreadMessages.length} unread messages!</h1>}
+            {unreadMessages.length > 0 && <h1>You have {unreadMessages.length} unread messages!</h1>}
+            {unreadMessages.length === 0 && <p>You have no unread messages!</p>}
         </div>
     )
 }
