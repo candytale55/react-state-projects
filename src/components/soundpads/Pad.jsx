d447f125fcfc35267efebffce6ba5976e239004a
CandyTale55 <candytale55@gmail.com>
2025-07-14 23:07:39 +0200

    feat: add toggle function to SoundPads
          Challenge: Create a toggle() function that logs
          "clicked!" to the console
          Pass that function down to each of the Pad components
          and set it up so when they get clicked, the function
          runs

--- src/components/soundpads/Pad.jsx
+++ src/components/soundpads/Pad.jsx
@@ -1,33 +1,16 @@
 import { useState } from "react"
 
 
-
-    /**
-     * Add a toggleAllOff button and function to prove that derived state 
-     * is not such a good idea. 
-     * The Pad components won't care if the button in SoundPads is pressed. 
-     * 
-     * The best choice is to have just one SOT.
-     * 
-     * Ref: 
-     * https://legacy.reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
-     * https://scrimba.com/learn-react-c0e/~03bb
-     * https://youtu.be/x4rFhThSX04?si=XJXCieNNE1oDYwCJ&t=31948
-     * 
-     */
-
-
-
 export default function Pad(props) {
 
     const [on, setOn] = useState(props.isOn)
     
-    function togglePad () { setOn(prevOn => !prevOn)}
+
     
     
     return (
         <button
-            onClick={togglePad}
+            onClick={props.handleClick}
             style={{ backgroundColor: props.padColor }}
             className={ on ? "on" : undefined }
         >
--- src/components/soundpads/SoundPads.jsx
+++ src/components/soundpads/SoundPads.jsx
@@ -8,16 +8,27 @@ import "./assets/styles.css"
 export default function SoundPads() {
   const [padData, setPadData] = useState(pads) // Step 1: Initialize state with the pads array
 
-  function turnAllPadsOff() {
-    console.log("Turning off")
-    setPads(prevPads => prevPads.map(pad => ({
-      ...pad,
-      on: false
-    })))
-  }
+  function togglePad() { console.log("clicked") }
+
+  /**
+   * Challenge: Create a toggle() function that logs
+   * "clicked!" to the console
+   * 
+   * Pass that function down to each of the Pad components
+   * and set it up so when they get clicked, the function runs
+  
+     * Ref: 
+     * https://legacy.reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+     * https://scrimba.com/learn-react-c0e/~03bb
+     * https://youtu.be/x4rFhThSX04?si=XJXCieNNE1oDYwCJ&t=31948
+     * 
+     */
+
+
 
   const padElements = padData.map(pad => 
     <Pad 
+      handleClick={togglePad}
       key={pad.id}
       padColor={pad.color}
       isOn={pad.on}
@@ -31,7 +42,6 @@ export default function SoundPads() {
             <div className="pad-container">
               {padElements}                
         </div>
-        {/*<button className="all-off" onClick={turnAllPadsOff}>Turn All Off</button>*/}
         </main>
     )
 }
