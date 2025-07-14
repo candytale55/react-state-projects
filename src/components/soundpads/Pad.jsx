599d775853039c7e5c4878a23fe2d5598e8e1e10
CandyTale55 <candytale55@gmail.com>
2025-07-14 22:27:07 +0200

    fix issue with Pad and SoundPads missing files

--- src/components/soundpads/Pad.jsx
+++ src/components/soundpads/Pad.jsx
@@ -1,11 +1,36 @@
+import { useState } from "react"
+
+
+
+/**
+ * Challenge: Create state controlling whether
+ * this pad is "on" or "off". Use the incoming
+ * `props.on` to determine the initial state.
+ * 
+ * Create an event listener so when the pad is clicked,
+ * it toggles from "on" to "off".
+ * 
+ * Goal: clicking each pad should toggle it on and off.
+ * 
+ * Ref: https://youtu.be/x4rFhThSX04?si=oQb2AeQEHG2OG-pj&t=31440
+ * https://scrimba.com/learn-react-c0e/~04md/s0lmhjavsm/head
+ * 
+ */
+
 
 
 export default function Pad(props) {
-    // console.log(props.padColor)
+
+    const [on, setOn] = useState(props.isOn)
+    
+    const toggle = (prevOn => !prevOn)
+    
+    
     return (
         <button
+            onClick={toggle}
             style={{ backgroundColor: props.padColor }}
-            className={ props.isOn ? "on" : undefined }
+            className={ on ? "on" : undefined }
         >
             
         </button>
--- src/components/soundpads/SoundPads.jsx
+++ src/components/soundpads/SoundPads.jsx
@@ -3,19 +3,7 @@ import Pad from "./Pad"
 import pads from "./assets/pads"
 import "./assets/styles.css"
 
-/**
- * Challenge: Create state controlling whether
- * this pad is "on" or "off". Use the incoming
- * `props.on` to determine the initial state.
- * 
- * Create an event listener so when the pad is clicked,
- * it toggles from "on" to "off".
- * 
- * Goal: clicking each pad should toggle it on and off.
- * 
- * Ref: https://scrimba.com/learn-react-c0e/~04md/s0lmhjavsm/head 
- * 
- */
+
 
 export default function SoundPads() {
   const [padData, setPadData] = useState(pads) // Step 1: Initialize state with the pads array
