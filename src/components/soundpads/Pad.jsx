01f53fccdf36f8b655c2e083dc22741866348ce7
CandyTale55 <candytale55@gmail.com>
2025-07-14 22:57:54 +0200

    references added to comment

--- src/components/soundpads/Pad.jsx
+++ src/components/soundpads/Pad.jsx
@@ -7,9 +7,12 @@ import { useState } from "react"
      * is not such a good idea. 
      * The Pad components won't care if the button in SoundPads is pressed. 
      * 
+     * The best choice is to have just one SOT.
      * 
      * Ref: 
      * https://legacy.reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html
+     * https://scrimba.com/learn-react-c0e/~03bb
+     * https://youtu.be/x4rFhThSX04?si=XJXCieNNE1oDYwCJ&t=31948
      * 
      */
 
